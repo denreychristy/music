6cb8e9b5dfe9e4e1d15f99cf1079a659f76dc5b2
Denrey Christy <denreyc@gmail.com>
2026-02-20 18:47:33 -0500

    2026-02-20-18-47

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ from modules.scale	import Scale
 
 # ================================================================================================ #
 
+class Song:
+	def __init__(self):
+		self.__sections: list[Section] = []
+
+class Section:
+	def __init__(self):
+		self.__measures: list[Measure] = []
+
+class Measure:
+	def __init__(self):
+		pass
+
+# ================================================================================================ #
+
 note = Note(60)
 scale = Scale(note, 'major')
 chord = Chord.from_scale(note, scale)
